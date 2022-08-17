935da5bb36194d59b911c7ca07609f537f9f9ab5
Brian Hardell <bharde@me.com>
2022-08-17 11:51:46 -0700

    init

---
+++ signal.py
@@ -0,0 +1,6 @@
+class Signal:
+    def __init__(self):
+        self.data = [] # x(n) sampled waveform 
+        self.sample_rate = 0.0 # (f) samples / second
+        self.period = 0 # (N) num of samples
+
